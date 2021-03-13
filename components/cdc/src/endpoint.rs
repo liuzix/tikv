9995c126a63dcafcd2f7292e14af48ff14dbb60a
Zixiong Liu <liuzixiong@pingcap.com>
2021-03-13 16:03:32 +0800

    WIP

--- components/cdc/src/endpoint.rs
+++ components/cdc/src/endpoint.rs
@@ -760,6 +760,7 @@ impl<T: 'static + RaftStoreRouter<RocksEngine>> Endpoint<T> {
             }
         };
         self.tso_worker.spawn(fut);
+        self.flush_all();
     }
 
     async fn region_resolved_ts_raft(
