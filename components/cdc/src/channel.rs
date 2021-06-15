f3bec5ed34a445ad2a72fa36cdd7471e7b658118
Zixiong Liu <liuzixiong@pingcap.com>
2021-06-15 18:22:36 +0800

    fix

--- components/cdc/src/channel.rs
+++ components/cdc/src/channel.rs
@@ -126,9 +126,7 @@ pub struct Sink {
 
 impl Sink {
     pub fn unbounded_send(&self, event: CdcEvent, force: bool) -> Result<(), SendError> {
-        fail_point!("cdc_inject_congestion", |_| {
-            Err(SendError::Congested)
-        });
+        fail_point!("cdc_inject_congestion", |_| Err(SendError::Congested));
         // Try it's best to send error events.
         let bytes = if !force { event.size() as usize } else { 0 };
         if bytes != 0 && !self.memory_quota.alloc(bytes) {
